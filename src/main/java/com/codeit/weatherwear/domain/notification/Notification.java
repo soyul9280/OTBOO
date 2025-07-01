e3a05a8cbca5b74a2da4a522ca1d4dbf2d1c8c5f
Oince <hky0105@naver.com>
2025-07-01 09:32:43 +0900

    feat: Notification 엔티티에 level 칼럼 추가

--- src/main/java/com/codeit/weatherwear/domain/notification/Notification.java
+++ src/main/java/com/codeit/weatherwear/domain/notification/Notification.java
@@ -3,12 +3,15 @@ package com.codeit.weatherwear.domain.notification;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -38,13 +41,28 @@ public class Notification {
   @Column(nullable = false)
   private String content;
 
-  private Notification(UUID receiverId, String title, String content) {
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Level level;
+
+  public enum Level {
+    INFO, WARNING, ERROR;
+  }
+
+  @Builder
+  private Notification(UUID receiverId, String title, String content, Level level) {
     this.receiverId = receiverId;
     this.title = title;
     this.content = content;
+    this.level = level;
   }
 
-  public static Notification create(UUID receiverId, String title, String content) {
-    return new Notification(receiverId, title, content);
+  public static Notification create(UUID receiverId, String title, String content, Level level) {
+    return Notification.builder()
+        .receiverId(receiverId)
+        .title(title)
+        .content(content)
+        .level(level)
+        .build();
   }
 }
