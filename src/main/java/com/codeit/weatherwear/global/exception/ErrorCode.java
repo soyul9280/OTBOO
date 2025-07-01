20ff001dbf0d49df932dbc92316f06ba3838c4f5
Soyoung Lee <soo7132@naver.com>
2025-07-01 18:37:54 +0900

    Style: 코드 스타일 설정 변경 #82

--- src/main/java/com/codeit/weatherwear/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/weatherwear/global/exception/ErrorCode.java
@@ -13,32 +13,32 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    // COMMON
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.", "관리자에게 연락해 주세요."),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "잘못된 요청을 진행하였습니다."),
+  // COMMON
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.", "관리자에게 연락해 주세요."),
+  INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "잘못된 요청을 진행하였습니다."),
 
-    // USER
-    USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "사용자 등록 실패", "사용자가 이미 존재합니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 확인 실패", "존재하지 않는 사용자입니다."),
+  // USER
+  USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "사용자 등록 실패", "사용자가 이미 존재합니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 확인 실패", "존재하지 않는 사용자입니다."),
 
-    //FOLLOW
-    SELF_FOLLOW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "자기 자신을 팔로우 할 수 없습니다.", ""),
-    FOLLOW_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 팔로우한 사용자입니다.", ""),
+  //FOLLOW
+  SELF_FOLLOW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "자기 자신을 팔로우 할 수 없습니다.", ""),
+  FOLLOW_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 팔로우한 사용자입니다.", ""),
 
-    // FEED
-    FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "피드 조회 실패", "존재하지 않는 피드입니다."),
-    UNSUPPORTED_SORT_FIELD(HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 필드", "지원하지 않는 정렬 필드입니다."),
-    NOT_IMPLEMENTED_SORT_FIELD(HttpStatus.NOT_IMPLEMENTED, "구현되지 않은 정렬 필드", "아직 구현되지 않은 정렬 필드입니다."),
-    INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "잘못된 Enum 필드", "잘못된 Enum 필드입니다."),
+  // FEED
+  FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "피드 조회 실패", "존재하지 않는 피드입니다."),
+  UNSUPPORTED_SORT_FIELD(HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 필드", "지원하지 않는 정렬 필드입니다."),
+  NOT_IMPLEMENTED_SORT_FIELD(HttpStatus.NOT_IMPLEMENTED, "구현되지 않은 정렬 필드", "아직 구현되지 않은 정렬 필드입니다."),
+  INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "잘못된 Enum 필드", "잘못된 Enum 필드입니다."),
 
-    // WEATHER
-    INVALID_WIND_SPEED(HttpStatus.BAD_REQUEST, "유효하지 않은 풍속", "풍속은 0 이상의 값이어야 합니다."),
+  // WEATHER
+  INVALID_WIND_SPEED(HttpStatus.BAD_REQUEST, "유효하지 않은 풍속", "풍속은 0 이상의 값이어야 합니다."),
 
-    // JWT
-    JWTSESSION_NOT_FOUND(HttpStatus.UNAUTHORIZED, "인증 정보 확인 실패", "토큰이 만료되거나 로그아웃되었습니다."),
-    INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT", "JWT 토큰이 손상되었거나 유효하지 않습니다.");
+  // JWT
+  JWTSESSION_NOT_FOUND(HttpStatus.UNAUTHORIZED, "인증 정보 확인 실패", "토큰이 만료되거나 로그아웃되었습니다."),
+  INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT", "JWT 토큰이 손상되었거나 유효하지 않습니다.");
 
-    private final HttpStatus status;
-    private final String message;
-    private final String detail;
+  private final HttpStatus status;
+  private final String message;
+  private final String detail;
 }
