d2ab2290821ae68602e315b3d6e7d4564ad2b702
Oince <hky0105@naver.com>
2025-07-04 17:05:47 +0900

    fix: 테스트 수정

--- src/test/java/com/codeit/weatherwear/domain/notification/NotificationServiceTest.java
+++ src/test/java/com/codeit/weatherwear/domain/notification/NotificationServiceTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +31,9 @@ class NotificationServiceTest {
 
   @Mock
   UserRepository userRepository;
+
+  @Mock
+  ApplicationEventPublisher eventPublisher;
 
   @InjectMocks
   NotificationService notificationService;
