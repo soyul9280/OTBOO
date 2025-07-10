f4cf299769f3a5b62bf754a4f4752989c137f9d5
soyul <soyul9280@gmail.com>
2025-07-10 16:09:14 +0900

    [Test]이미지의상등록테스트

--- src/test/java/com/codeit/weatherwear/domain/clothes/service/ClothServiceTest.java
+++ src/test/java/com/codeit/weatherwear/domain/clothes/service/ClothServiceTest.java
@@ -29,6 +29,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -36,226 +37,262 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 public class ClothServiceTest {
 
-    @Mock
-    private AttributeRepository attributeRepository;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private ClothRepository clothRepository;
-    @Mock
-    private ThumbnailImageStorage thumbnailImageStorage;
-    @Mock
-    private ClothMapper mapper;
-    @Mock
-    private List<SiteParser> siteParsers;
+  @Mock
+  private AttributeRepository attributeRepository;
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private ClothRepository clothRepository;
+  @Mock
+  private ThumbnailImageStorage thumbnailImageStorage;
+  @Mock
+  private ClothMapper mapper;
+  @Mock
+  private List<SiteParser> siteParsers;
 
-    @InjectMocks
-    private ClothServiceImpl sut;
+  @InjectMocks
+  private ClothServiceImpl sut;
 
-    @Nested
-    @DisplayName("의상 등록 테스트 - 이미지 없음")
-    class RegisterCloth {
+  UUID ownerId;
+  UUID clothesId;
+  UUID colorId;
+  UUID sizeId;
+  User mockUser;
+  Attribute colorDef;
+  Attribute sizeDef;
+  Cloth cloth;
 
-        @Test
-        @DisplayName("등록 성공")
-        void create_success() {
-            //given
-            UUID ownerId = UUID.randomUUID();
-            UUID clothesId = UUID.randomUUID();
-            UUID colorId = UUID.randomUUID();
-            UUID sizeId = UUID.randomUUID();
 
-            ClothesAttributeDto color = new ClothesAttributeDto(colorId, "파랑");
-            ClothesAttributeDto size = new ClothesAttributeDto(sizeId, "S");
-            ClothesCreateRequest request = new ClothesCreateRequest(
-                ownerId,
-                "후드티",
-                ClothType.TOP,
-                List.of(color, size)
-            );
-            ClothesAttributeWithDefDto colorDto = new ClothesAttributeWithDefDto(colorId,
-                "색상", List.of("빨강", "파랑"), "파랑");
-            ClothesAttributeWithDefDto sizeDto = new ClothesAttributeWithDefDto(sizeId,
-                "사이즈", List.of("S", "L"), "S");
+  @BeforeEach
+  void setUp() {
+    ownerId = UUID.randomUUID();
+    clothesId = UUID.randomUUID();
+    colorId = UUID.randomUUID();
+    sizeId = UUID.randomUUID();
 
-            Attribute colorDef = Attribute.builder()
-                .id(colorId)
-                .name("색상")
-                .selectableValues(List.of("빨강", "파랑"))
-                .build();
-            Attribute sizeDef = Attribute.builder()
-                .id(sizeId)
-                .name("사이즈")
-                .selectableValues(List.of("S", "L"))
-                .build();
+    mockUser = User.builder().id(ownerId).build();
+    colorDef = Attribute.builder().id(colorId).name("색상").selectableValues(List.of("빨강", "파랑"))
+        .build();
+    sizeDef = Attribute.builder().id(sizeId).name("사이즈").selectableValues(List.of("S", "L"))
+        .build();
+    cloth = Cloth.builder()
+        .id(clothesId)
+        .createdAt(Instant.now())
+        .updatedAt(Instant.now())
+        .name("후드티")
+        .clothType(ClothType.TOP)
+        .clothesImageUrl(null)
+        .user(mockUser)
+        .build();
+  }
 
-            ClothesDto clothesDto = ClothesDto.builder()
-                .id(clothesId)
-                .ownerId(ownerId)
-                .name("후드티")
-                .imageUrl(null)
-                .type(ClothType.TOP)
-                .attributes(List.of(colorDto, sizeDto))
-                .build();
 
-            Cloth clothes = Cloth.builder()
-                .id(clothesId)
-                .createdAt(Instant.now())
-                .updatedAt(Instant.now())
-                .name("후드티")
-                .clothType(ClothType.TOP)
-                .clothesImageUrl(null)
-                .user(User.builder().id(ownerId).build())
-                .build();
+  @Nested
+  @DisplayName("의상 등록 테스트")
+  class RegisterCloth {
 
-            User mockUser = User.builder().id(ownerId).build();
-            given(userRepository.findById(ownerId)).willReturn(Optional.of(mockUser));
+    @Test
+    @DisplayName("직접 등록 성공 - 이미지 없음")
+    void create_success() {
+      //given
+      ClothesAttributeDto color = new ClothesAttributeDto(colorId, "파랑");
+      ClothesAttributeDto size = new ClothesAttributeDto(sizeId, "S");
+      ClothesCreateRequest request = new ClothesCreateRequest(
+          ownerId,
+          "후드티",
+          ClothType.TOP,
+          List.of(color, size)
+      );
+      ClothesAttributeWithDefDto colorDto = new ClothesAttributeWithDefDto(colorId,
+          "색상", List.of("빨강", "파랑"), "파랑");
+      ClothesAttributeWithDefDto sizeDto = new ClothesAttributeWithDefDto(sizeId,
+          "사이즈", List.of("S", "L"), "S");
 
-            given(attributeRepository.findAllById(any())).willReturn(List.of(colorDef, sizeDef));
-            given(clothRepository.save(any(Cloth.class))).willReturn(clothes);
-            given(mapper.toDto(any(Cloth.class),any())).willReturn(clothesDto);
-            //when
-            ClothesDto result = sut.create(request,null);
-            //then
-            assertThat(result.getName()).isEqualTo("후드티");
-            assertThat(result.getType()).isEqualTo(ClothType.TOP);
-            assertThat(result.getAttributes().get(0).value()).isEqualTo("파랑");
-            assertThat(result.getAttributes().get(1).value()).isEqualTo("S");
-            verify(clothRepository, times(1)).save(any(Cloth.class));
-        }
+      ClothesDto clothesDto = ClothesDto.builder()
+          .id(clothesId)
+          .ownerId(ownerId)
+          .name("후드티")
+          .imageUrl(null)
+          .type(ClothType.TOP)
+          .attributes(List.of(colorDto, sizeDto))
+          .build();
 
-        @Test
-        @DisplayName("의상 등록 실패 - 속성ID가 존재하지 않을 경우")
-        void create_fail() {
-            //given
-            UUID ownerId = UUID.randomUUID();
-            UUID attributesId = UUID.randomUUID();
-            ClothesCreateRequest request = new ClothesCreateRequest(ownerId, "후드티",
-                ClothType.TOP,
-                List.of(new ClothesAttributeDto(attributesId, "파랑"))
-            );
-
-            User mockUser = User.builder().id(ownerId).build();
-            given(userRepository.findById(ownerId)).willReturn(Optional.of(mockUser));
-            given(attributeRepository.findAllById(any())).willReturn(List.of()); // 속성 없음
-
-            // when & then
-            assertThatThrownBy(() -> sut.create(request,null))
-                .isInstanceOf(AttributeNotFoundException.class)
-                .hasMessageContaining("속성 확인 실패");
-
-            verify(clothRepository, never()).save(any());
-        }
+      given(userRepository.findById(ownerId)).willReturn(Optional.of(mockUser));
+      given(attributeRepository.findAllById(any())).willReturn(List.of(colorDef, sizeDef));
+      given(clothRepository.save(any(Cloth.class))).willReturn(cloth);
+      given(mapper.toDto(any(Cloth.class), any())).willReturn(clothesDto);
+      //when
+      ClothesDto result = sut.create(request, null);
+      //then
+      assertThat(result.getName()).isEqualTo("후드티");
+      assertThat(result.getType()).isEqualTo(ClothType.TOP);
+      assertThat(result.getAttributes().get(0).value()).isEqualTo("파랑");
+      assertThat(result.getAttributes().get(1).value()).isEqualTo("S");
+      verify(clothRepository, times(1)).save(any(Cloth.class));
+      verify(thumbnailImageStorage, never()).upload(any());
     }
 
-    @Nested
-    @DisplayName("의상 삭제 테스트")
-    class DeleteCloth {
+    @Test
+    @DisplayName("직접 의상 등록 실패 - 속성ID가 존재하지 않을 경우")
+    void create_fail() {
+      //given
+      UUID attributesId = UUID.randomUUID();
+      ClothesCreateRequest request = new ClothesCreateRequest(ownerId, "후드티",
+          ClothType.TOP,
+          List.of(new ClothesAttributeDto(attributesId, "파랑"))
+      );
 
-        @Test
-        @DisplayName("삭제 성공")
-        void delete_success() {
-            //given
-            UUID ownerId = UUID.randomUUID();
-            UUID clothesId = UUID.randomUUID();
-            Cloth clothes = Cloth.builder()
-                .id(clothesId)
-                .createdAt(Instant.now())
-                .updatedAt(Instant.now())
-                .name("후드티")
-                .clothType(ClothType.TOP)
-                .clothesImageUrl(null)
-                .user(User.builder().id(ownerId).build())
-                .build();
+      given(userRepository.findById(ownerId)).willReturn(Optional.of(mockUser));
+      given(attributeRepository.findAllById(any())).willReturn(List.of()); // 속성 없음
 
-            given(clothRepository.findById(clothesId)).willReturn(Optional.of(clothes));
+      // when & then
+      assertThatThrownBy(() -> sut.create(request, null))
+          .isInstanceOf(AttributeNotFoundException.class)
+          .hasMessageContaining("속성 확인 실패");
 
-            //when
-            sut.delete(clothesId);
-            //then
-            verify(clothRepository, times(1)).findById(clothesId);
-            verify(clothRepository, times(1)).delete(clothes);
-        }
-
-        @Test
-        @DisplayName("삭제 실패 - 존재하지 않는 의상")
-        void delete_fail() {
-            // given
-            UUID id = UUID.randomUUID();
-            given(clothRepository.findById(id)).willReturn(Optional.empty());
-
-            // when & then
-            assertThatThrownBy(() -> sut.delete(id))
-                .isInstanceOf(ClothNotFoundException.class)
-                .hasMessageContaining("옷 확인 실패");
-            verify(clothRepository, times(1)).findById(id);
-            verify(clothRepository, never()).delete(any());
-        }
+      verify(clothRepository, never()).save(any());
+      verify(thumbnailImageStorage, never()).upload(any());
     }
 
-    @Nested
-    @DisplayName("의상 수정 테스트 - 이미지 없음")
-    class UpdateCloth {
+    @Test
+    @DisplayName("직접 의상 등록 - 이미지 존재")
+    void create_withImage() {
+      //given
+      ClothesAttributeDto color = new ClothesAttributeDto(colorId, "파랑");
+      ClothesAttributeDto size = new ClothesAttributeDto(sizeId, "S");
+      ClothesCreateRequest request = new ClothesCreateRequest(
+          ownerId,
+          "후드티",
+          ClothType.TOP,
+          List.of(color, size)
+      );
+      MultipartFile file = new MockMultipartFile("file", "image.jpg", "image/jpeg",
+          "image-content".getBytes());
+      String imageUrl = "https://s3.com/image.jpg";
+      String imageKey = "image-key";
 
-        @Test
-        @DisplayName("수정 성공")
-        void update_success() {
-            //given
-            UUID ownerId = UUID.randomUUID();
-            UUID clothesId = UUID.randomUUID();
-            UUID colorId = UUID.randomUUID();
+      Cloth clothWithImage = Cloth.builder()
+          .id(clothesId)
+          .createdAt(Instant.now())
+          .updatedAt(Instant.now())
+          .name("후드티")
+          .clothType(ClothType.TOP)
+          .clothesImageUrl(imageUrl)
+          .user(mockUser)
+          .build();
 
-            ClothesAttributeWithDefDto colorDto = new ClothesAttributeWithDefDto(colorId,
-                "색상", List.of("빨강", "파랑"), "빨강");
+      ClothesAttributeWithDefDto colorDto = new ClothesAttributeWithDefDto(colorId,
+          "색상", List.of("빨강", "파랑"), "파랑");
+      ClothesAttributeWithDefDto sizeDto = new ClothesAttributeWithDefDto(sizeId,
+          "사이즈", List.of("S", "L"), "S");
 
-            Attribute colorDef = Attribute.builder()
-                .id(colorId)
-                .name("색상")
-                .selectableValues(List.of("빨강", "파랑"))
-                .build();
+      ClothesDto clothesDto = ClothesDto.builder()
+          .id(clothesId)
+          .ownerId(ownerId)
+          .name("후드티")
+          .imageUrl(imageUrl)
+          .type(ClothType.TOP)
+          .attributes(List.of(colorDto, sizeDto))
+          .build();
 
-            Cloth cloth = Cloth.builder()
-                .id(clothesId)
-                .createdAt(Instant.now())
-                .updatedAt(Instant.now())
-                .name("후드티")
-                .clothType(ClothType.TOP)
-                .clothesImageUrl(null)
-                .user(User.builder().id(ownerId).build())
-                .build();
-
-            ClothesDto clothesDto = ClothesDto.builder()
-                .id(clothesId)
-                .ownerId(ownerId)
-                .name("빨강 후드티")
-                .imageUrl(null)
-                .type(ClothType.TOP)
-                .attributes(List.of(colorDto))
-                .build();
-
-            ClothesUpdateRequest request = new ClothesUpdateRequest(
-                "빨강 후드티",
-                ClothType.TOP,
-                List.of(
-                    new ClothesAttributeDto(colorId, "빨강")
-                )
-            );
-
-            given(clothRepository.findByIdWithAttributes(clothesId)).willReturn(Optional.of(cloth));
-            given(attributeRepository.findAllById(any())).willReturn(List.of(colorDef));
-            given(mapper.toDto(any(Cloth.class),any())).willReturn(clothesDto);
-            //when
-            ClothesDto result=sut.update(clothesId,request,null);
-
-            //then
-            assertThat(result.getName()).isEqualTo("빨강 후드티");
-            assertThat(result.getType()).isEqualTo(ClothType.TOP);
-            assertThat(result.getAttributes().get(0).value()).isEqualTo("빨강");
-            verify(attributeRepository, times(1)).findAllById(any());
-        }
+      given(userRepository.findById(ownerId)).willReturn(Optional.of(mockUser));
+      given(thumbnailImageStorage.upload(file)).willReturn(imageKey);
+      given(thumbnailImageStorage.get(imageKey)).willReturn(imageUrl);
+      given(attributeRepository.findAllById(any())).willReturn(List.of(colorDef, sizeDef));
+      given(clothRepository.save(any(Cloth.class))).willReturn(clothWithImage);
+      given(mapper.toDto(any(Cloth.class), any())).willReturn(clothesDto);
+      //when
+      ClothesDto result = sut.create(request, file);
+      //then
+      assertThat(result.getName()).isEqualTo("후드티");
+      assertThat(result.getType()).isEqualTo(ClothType.TOP);
+      assertThat(result.getAttributes().get(0).value()).isEqualTo("파랑");
+      assertThat(result.getAttributes().get(1).value()).isEqualTo("S");
+      verify(clothRepository, times(1)).save(any(Cloth.class));
+      verify(thumbnailImageStorage).upload(file);
+      verify(thumbnailImageStorage).get(imageKey);
     }
+  }
+
+  @Nested
+  @DisplayName("의상 삭제 테스트")
+  class DeleteCloth {
+
+    @Test
+    @DisplayName("삭제 성공 - 이미지가 없는 의상")
+    void delete_success() {
+      //given
+      given(clothRepository.findById(clothesId)).willReturn(Optional.of(cloth));
+
+      //when
+      sut.delete(clothesId);
+      //then
+      verify(clothRepository, times(1)).findById(clothesId);
+      verify(clothRepository, times(1)).delete(cloth);
+    }
+
+    @Test
+    @DisplayName("삭제 실패 - 존재하지 않는 의상")
+    void delete_fail() {
+      // given
+      UUID id = UUID.randomUUID();
+      given(clothRepository.findById(id)).willReturn(Optional.empty());
+
+      // when & then
+      assertThatThrownBy(() -> sut.delete(id))
+          .isInstanceOf(ClothNotFoundException.class)
+          .hasMessageContaining("옷 확인 실패");
+      verify(clothRepository, times(1)).findById(id);
+      verify(clothRepository, never()).delete(any());
+    }
+  }
+
+  @Nested
+  @DisplayName("의상 수정 테스트")
+  class UpdateCloth {
+
+    @Test
+    @DisplayName("수정 성공 - 이미지 없음")
+    void update_success() {
+      //given
+      ClothesAttributeWithDefDto colorDto = new ClothesAttributeWithDefDto(colorId,
+          "색상", List.of("빨강", "파랑"), "빨강");
+
+      ClothesUpdateRequest request = new ClothesUpdateRequest(
+          "빨강 후드티",
+          ClothType.TOP,
+          List.of(
+              new ClothesAttributeDto(colorId, "빨강")
+          )
+      );
+
+      ClothesDto clothesDto = ClothesDto.builder()
+          .id(clothesId)
+          .ownerId(ownerId)
+          .name("빨강 후드티")
+          .imageUrl(null)
+          .type(ClothType.TOP)
+          .attributes(List.of(colorDto))
+          .build();
+
+      given(clothRepository.findByIdWithAttributes(clothesId)).willReturn(Optional.of(cloth));
+      given(attributeRepository.findAllById(any())).willReturn(List.of(colorDef));
+      given(mapper.toDto(any(Cloth.class), any())).willReturn(clothesDto);
+      //when
+      ClothesDto result = sut.update(clothesId, request, null);
+
+      //then
+      assertThat(result.getName()).isEqualTo("빨강 후드티");
+      assertThat(result.getType()).isEqualTo(ClothType.TOP);
+      assertThat(result.getAttributes().get(0).value()).isEqualTo("빨강");
+      verify(attributeRepository, times(1)).findAllById(any());
+      verify(thumbnailImageStorage, never()).upload(any());
+    }
+  }
 }
