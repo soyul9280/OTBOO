f7af51fdcab79cb000c00c16bc7b4b8d97da7040
Soyoung Lee <soo7132@naver.com>
2025-07-11 16:17:24 +0900

    Refactor: 이메일 전송 비동기 적용 #148

--- src/main/java/com/codeit/weatherwear/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/codeit/weatherwear/domain/auth/service/AuthServiceImpl.java
@@ -9,8 +9,6 @@ import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +20,7 @@ public class AuthServiceImpl implements AuthService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
-  private final JavaMailSender javaMailSender;
+  private final EmailService emailService;
   private final SecureRandom secureRandom = new SecureRandom();
   private final static String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678";
 
@@ -42,7 +40,7 @@ public class AuthServiceImpl implements AuthService {
     String tempPassword = generateTempPassword();
 
     // 이메일 전송
-    sendTempPasswordEmail(email, tempPassword);
+    emailService.sendTempPasswordEmail(email, tempPassword);
 
     // DB 업데이트
     String encodedTempPassword = passwordEncoder.encode(tempPassword);
@@ -61,28 +59,5 @@ public class AuthServiceImpl implements AuthService {
     return stringBuffer.toString();
   }
 
-  private void sendTempPasswordEmail(String toEmail, String tempPassword) {
-    String content = """
-        안녕하세요.
-        "옷장을 부탁해" 서비스입니다.
-                
-        요청하신 임시 비밀번호는 아래와 같습니다.
-        --------------
-        %s
-        --------------
-                
-        임시 비밀번호는 발급 시점으로부터 10분 간 유효합니다.
-        로그인 후 반드시 비밀번호를 변경해주세요.
-                
-        감사합니다.        
-        """.formatted(tempPassword);
-
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(toEmail);
-    message.setSubject("[옷장을 부탁해] 임시 비밀번호 안내");
-    message.setText(content);
-
-    javaMailSender.send(message);
-  }
 
 }
