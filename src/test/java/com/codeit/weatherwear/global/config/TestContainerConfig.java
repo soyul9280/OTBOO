dd4d7b3d6475d1f80d2980e5653eac63a0089b0e
Oince <hky0105@naver.com>
2025-07-17 17:58:58 +0900

    feat: 컨테이너를 하나만 생성 후 공유하도록 변경

--- src/test/java/com/codeit/weatherwear/global/config/TestContainerConfig.java
+++ src/test/java/com/codeit/weatherwear/global/config/TestContainerConfig.java
@@ -8,13 +8,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfig {
 
-  @Bean
-  @ServiceConnection
-  PostgreSQLContainer<?> postgreSQLContainer() {
-    return new PostgreSQLContainer<>("postgres:17-alpine")
+  private static final PostgreSQLContainer<?> POSTGRES;
+
+  static {
+    POSTGRES = new PostgreSQLContainer<>("postgres:17-alpine")
         .withDatabaseName("test")
         .withUsername("test_user")
         .withPassword("test_password")
-        .withReuse(true);
+        .withEnv("POSTGRES_SHUTDOWN_TIMEOUT", "5");
+  }
+
+  @Bean(destroyMethod = "stop")
+  @ServiceConnection
+  public PostgreSQLContainer<?> postgreSQLContainer() {
+    return POSTGRES;
   }
 }
