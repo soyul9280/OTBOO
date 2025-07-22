c89d92e72527cb1c76bf77c5177ec4629b78e8cc
Oince <hky0105@naver.com>
2025-07-22 10:41:02 +0900

    chore: Notification 도메인 api 문서 작성

--- src/main/java/com/codeit/weatherwear/domain/notification/dto/NotificationDto.java
+++ src/main/java/com/codeit/weatherwear/domain/notification/dto/NotificationDto.java
@@ -2,9 +2,11 @@ package com.codeit.weatherwear.domain.notification.dto;
 
 import com.codeit.weatherwear.domain.notification.Notification;
 import com.codeit.weatherwear.domain.notification.Notification.Level;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.Instant;
 import java.util.UUID;
 
+@Schema(name = "알림 정보")
 public record NotificationDto(
     UUID id,
     Instant createdAt,
