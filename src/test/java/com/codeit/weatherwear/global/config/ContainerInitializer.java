e310303237c9becf8fa0cc2e3fc0b1087e3705af
Soyoung Lee <soo7132@naver.com>
2025-07-25 12:58:05 +0900

    Test: 테스트마다 별도의 컨테이너

--- src/test/java/com/codeit/weatherwear/global/config/ContainerInitializer.java
+++ src/test/java/com/codeit/weatherwear/global/config/ContainerInitializer.java
@@ -9,27 +9,31 @@ import org.testcontainers.kafka.KafkaContainer;
 public class ContainerInitializer implements
     ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-  private static final PostgreSQLContainer<?> POSTGRES =
-      new PostgreSQLContainer<>("postgres:17-alpine")
-          .withDatabaseName("weatherwear")
-          .withUsername("user")
-          .withPassword("password")
-          .withReuse(false);
-
-  private static final KafkaContainer KAFKA = new KafkaContainer("apache/kafka:4.0.0");
-
-  static {
-    POSTGRES.start();
-    KAFKA.start();
-  }
-
   @Override
   public void initialize(ConfigurableApplicationContext applicationContext) {
+    PostgreSQLContainer<?> postgres =
+        new PostgreSQLContainer<>("postgres:17-alpine")
+            .withDatabaseName("weatherwear")
+            .withUsername("user")
+            .withPassword("password")
+            .withReuse(false);
+    KafkaContainer kafka = new KafkaContainer("apache/kafka:4.0.0")
+        .withReuse(false);
+    postgres.start();
+    kafka.start();
+
+    // 애플리케이션 컨텍스트가 닫힐 때 컨테이너 정리
+    applicationContext.registerShutdownHook();
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      postgres.stop();
+      kafka.stop();
+    }));
+
     TestPropertyValues.of(
-        "spring.datasource.url=" + POSTGRES.getJdbcUrl(),
-        "spring.datasource.username=" + POSTGRES.getUsername(),
-        "spring.datasource.password=" + POSTGRES.getPassword(),
-        "spring.kafka.bootstrap-servers=" + KAFKA.getBootstrapServers()
+        "spring.datasource.url=" + postgres.getJdbcUrl(),
+        "spring.datasource.username=" + postgres.getUsername(),
+        "spring.datasource.password=" + postgres.getPassword(),
+        "spring.kafka.bootstrap-servers=" + kafka.getBootstrapServers()
     ).applyTo(applicationContext.getEnvironment());
   }
 }
