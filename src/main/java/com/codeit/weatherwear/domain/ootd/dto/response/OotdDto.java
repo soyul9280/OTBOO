93f7a392f2c0f2d2fcf46cf47924e0be2acffc34
silvarge <sep.non2@gmail.com>
2025-07-28 14:15:17 +0900

    Fix: 피드 상세 조회 시 속성 표시 되지 않은 부분 수정 #235

--- src/main/java/com/codeit/weatherwear/domain/ootd/dto/response/OotdDto.java
+++ src/main/java/com/codeit/weatherwear/domain/ootd/dto/response/OotdDto.java
@@ -1,6 +1,6 @@
 package com.codeit.weatherwear.domain.ootd.dto.response;
 
-import com.codeit.weatherwear.domain.clothes.dto.response.ClothesAttributeDefDto;
+import com.codeit.weatherwear.domain.clothes.dto.response.ClothesAttributeWithDefDto;
 import java.util.List;
 import java.util.UUID;
 import lombok.Builder;
@@ -14,5 +14,5 @@ public class OotdDto {
   private final String name;
   private final String imageUrl;
   private final String type;
-  private final List<ClothesAttributeDefDto> attributes;
+  private final List<ClothesAttributeWithDefDto> attributes;
 }
--- src/main/java/com/codeit/weatherwear/domain/ootd/mapper/OotdMapper.java
+++ src/main/java/com/codeit/weatherwear/domain/ootd/mapper/OotdMapper.java
@@ -1,11 +1,12 @@
 package com.codeit.weatherwear.domain.ootd.mapper;
 
+import com.codeit.weatherwear.domain.clothes.dto.response.ClothesAttributeWithDefDto;
 import com.codeit.weatherwear.domain.clothes.entity.Cloth;
-import com.codeit.weatherwear.domain.clothes.mapper.ClothMapper;
 import com.codeit.weatherwear.domain.feed.entity.Feed;
 import com.codeit.weatherwear.domain.ootd.dto.response.OotdDto;
 import com.codeit.weatherwear.domain.ootd.entity.Ootd;
 import com.codeit.weatherwear.global.storage.ThumbnailImageStorage;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OotdMapper {
 
-  private final ClothMapper clothMapper;
   private final ThumbnailImageStorage thumbnailImageStorage;
 
   public Ootd toEntity(Feed feed, Cloth cloth) {
@@ -29,12 +29,22 @@ public class OotdMapper {
         cloth.getClothesImageUrl() != null ? thumbnailImageStorage.get(cloth.getClothesImageUrl())
             : null;
 
+    List<ClothesAttributeWithDefDto> clothesAttributeWithDefDtos = cloth.getClothesWithAttributes()
+        .stream()
+        .map(attr -> new ClothesAttributeWithDefDto(
+            attr.getAttribute().getId(),
+            attr.getAttribute().getName(),
+            attr.getAttribute().getSelectableValues(),
+            attr.getValue()
+        ))
+        .toList();
+
     return OotdDto.builder()
         .clothesId(cloth.getId())
         .name(cloth.getName())
         .imageUrl(imageUrl)
         .type(cloth.getClothType().toString())
-        .attributes(clothMapper.toAttributeDefDto(cloth.getClothesWithAttributes()))
+        .attributes(clothesAttributeWithDefDtos)
         .build();
   }
 
