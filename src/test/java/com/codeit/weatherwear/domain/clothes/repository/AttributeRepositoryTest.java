b53a83ccc275509bc5ad516b7e436308fcbd4b02
Oince <hky0105@naver.com>
2025-07-28 18:00:43 +0900

    refactor: 주석 제거

--- src/test/java/com/codeit/weatherwear/domain/clothes/repository/AttributeRepositoryTest.java
+++ src/test/java/com/codeit/weatherwear/domain/clothes/repository/AttributeRepositoryTest.java
@@ -35,7 +35,6 @@ class AttributeRepositoryTest {
 
   @BeforeEach
   void setUp() {
-//    sut.deleteAll();
     thick = Attribute.builder()
         .name("두께")
         .selectableValues(List.of("두꺼움", "얇음")).build();
